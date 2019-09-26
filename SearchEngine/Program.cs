6dbdd620bd7fa692fba666425542e70953ea3995
Jacob6565 <jacob.gosch97@gmail.com>
2019-09-26 16:14:59 +0200

    tilføjede tekst til hvordan man kunne gøre det concurrent.

--- SearchEngine/Program.cs
+++ SearchEngine/Program.cs
@@ -9,7 +9,24 @@ using SearchEngine.WebCrawler;
 namespace SearchEngine
 {
     public class Program
-    {       
+    {
+        //Så for at lave det concurrent, skal jeg sikre mig, at jeg håndterer
+        //at de ikke tilgå det samme domæne på samme tid i.e. har flere end en 
+        //connection åben til et domæne af gangen
+
+        //Derudover kan man benytte sig at concurrency dictionaries osv., som gør
+        //at operationerne dertil er atomare. Dog ikke for lists. Men jeg tænker 
+        //at man kan lave en ny list klasse, som nedarver fra den normale list klasse.
+        //Den nye har så sin egen add funktion (AddNew), som blot er lig den gamles add 
+        //funktion blot indkapslet i en semaphor, så man sikrer at kun en adder af gangen:
+
+        //NewAdd(string item)
+        //  semaphor-start
+        //  base.Add(item)
+        //  semaphor-release
+
+        //og det samme kunne man gøre for remove, osv:
+
         static void Main(string[] args)
         {
             WebCrawler.WebCrawler webCrawler = new WebCrawler.WebCrawler();
