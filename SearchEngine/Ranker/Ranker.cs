9096e3697f7f3616f6ab7e793afe8c30fa268946
Jacob6565 <jacob.gosch97@gmail.com>
2019-10-05 19:27:51 +0200

    ranker should be working now.

--- SearchEngine/Ranker/Ranker.cs
+++ SearchEngine/Ranker/Ranker.cs
@@ -4,25 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SearchEngine.Indexer;
+using SearchEngine.WebCrawler;
 
 namespace SearchEngine.Ranker
 {
     public class Ranker
     {
         //docID --> score
-        public Dictionary<int, int> Score = new Dictionary<int, int>();
+        public Dictionary<int, double> Score = new Dictionary<int, double>();
         public Indexer.Indexer indexer;
+        public List<int> IdsOfMatchedDocuments;
+        public List<string> termsInQuery;
+        public PageDB pageDB;
 
-        public Ranker(Indexer.Indexer indexer)
+        public List<int> Top10Documents(Indexer.Indexer indexer,                                        
+                                        List<string> query,
+                                        PageDB pageDB)
         {
-            //vi skal jo have adgang til indexet.
             this.indexer = indexer;
-        }
+            this.termsInQuery = query;
+            this.pageDB = pageDB;
 
-
-        public List<int> Top10Documents()
-        {
-
+            CalculateDocumentScores(query);
             //have en foreach, hvor man extracter vægtene for alle terms i doci, 
             //men kun de terms som også er i query t, de andre er jo irrelevante.
             //og med vægte mener jeg, at man udregner tf-idf value ud fra tf og df værdierne
@@ -30,8 +33,68 @@ namespace SearchEngine.Ranker
 
             //det samme gør man så for querien, altså udregner tf-idf værdien 
             //for de terms der indgår i den querien.
-            return Score.Values.OrderByDescending(x => x).Take(10).ToList(); 
-        } 
+            return Score.OrderByDescending(x => x.Value).Select(x=> x.Key).Take(10).ToList();
+        }
 
+        public void CalculateDocumentScores(List<string> termsFromQuery)
+        {
+            //length of file interms of how many terms it contains
+            int numberOfDocuments = pageDB.GetNumOfCrawledPages();
+            
+            foreach (string term in termsFromQuery)
+            {
+                Console.WriteLine("term");
+                //retrieve postings for term.
+                List<Tuple<int, int>> postings = new List<Tuple<int, int>>();
+                try
+                {
+                    postings = indexer.indexCreator.index[term];
+                }
+                catch(Exception)
+                {
+                    //så hvis der ikke er nogle dokumenter som indeholder termen.
+                    continue;
+                }
+
+                //df is equal to the length of the postings.
+                int df = postings.Count;
+                double Qtfidf = CalculateTfIdfForQuery(term, termsFromQuery, numberOfDocuments, df);
+
+                //for each documents in that postings, i.e. the documents containing the term
+                //we calculate the score it gets for this term
+                foreach (Tuple<int, int> docTfPair in postings)
+                {
+                    if (!Score.ContainsKey(docTfPair.Item1))
+                    {
+                        Score.Add(docTfPair.Item1, 0);
+                    }
+
+                    double Dtfidf = CalculateTfIdfForDocument(docTfPair, numberOfDocuments, df);
+
+                    Score[docTfPair.Item1] += Qtfidf * Dtfidf;
+                    int numberOftermsInDoc = indexer.IdToTerms[docTfPair.Item1].Count;
+                    Score[docTfPair.Item1] = Score[docTfPair.Item1] / numberOftermsInDoc;
+                }
+            }
+        }
+
+
+        private double CalculateTfIdfForDocument(Tuple<int, int> docTfPair,
+                                              int numberOfDocuments, int df)
+        {
+            int tf = docTfPair.Item2;
+            double idf = Math.Log10(numberOfDocuments / df);
+            return tf * idf;
+
+        }
+
+        private double CalculateTfIdfForQuery(string term, List<string> termsFromQuery,
+                                           int numberOfDocuments, int df)
+        {
+            int tf = termsFromQuery.Where(x => x == term).Count();
+            double idf = Math.Log10(numberOfDocuments / df);
+
+            return tf*idf;
+        }
     }
 }
